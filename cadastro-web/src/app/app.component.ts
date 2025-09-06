cd29e9bf152f60bc21699e077fd93aeec2168379
Flavio Santos <devflaviosantos@gmail.com>
2025-09-06 13:53:05 -0300

    ajuste na pasta cadastro-web

---
+++ cadastro-web/src/app/app.component.ts
@@ -0,0 +1,90 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Proposta } from './model/proposta';
+import { CadastroService } from './service/cadastro.service';
+import { WebSocketConnector } from './service/web-socket-connector';
+import { ToastrService } from 'ngx-toastr';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  exibirTelaCadastro = false
+  observacao: string = ''
+  exibirMsgCampoObrigatorio: boolean = false
+  propostas: Proposta[] = []
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private cadastroService: CadastroService,
+    private notificacao: ToastrService
+  ) {
+    new WebSocketConnector('http://localhost:8080/ws', '/propostas', this.onMessage.bind(this));
+  }
+
+  ngOnInit(): void {
+    this.buscarPropostas();
+  }
+
+  formulario: FormGroup = this.formBuilder.group({
+    nome: ['', [Validators.required, Validators.minLength(3)]],
+    sobrenome: ['', [Validators.required, Validators.minLength(3)]],
+    telefone: ['', [Validators.required, Validators.minLength(3)]],
+    renda: [null, [Validators.required, Validators.minLength(1)]],
+    cpf: ['', [Validators.required, Validators.minLength(11)]],
+    valorSolicitado: [null, [Validators.required, Validators.minLength(1)]],
+    prazoPagamento: [24],
+  })
+
+  buscarPropostas(): void {
+    this.cadastroService.obterPropostas().subscribe({
+      next: propostas => {
+        this.propostas = propostas
+      }, error: () => {
+        this.notificacao.error('Erro ao carregar propostas')
+      }
+    })
+  }
+
+  cadastrarProposta(): void {
+    if (this.formulario.invalid) {
+      this.exibirMsgCampoObrigatorio = true;
+      return;
+    }
+
+    this.cadastroService.castrarProposta(this.formulario.value).subscribe({
+      next: () => {
+        this.buscarPropostas()
+        this.limparFormulario();
+        this.notificacao.success('Proposta cadastrada com sucesso!')
+      }, error: (error) => {
+        this.notificacao.error(error.error)
+      }
+    })
+  }
+
+  limparFormulario(): void {
+    this.formulario.patchValue({
+      nome: '',
+      telefone: '',
+      sobrenome: '',
+      cpf: '',
+      prazoPagamento: 24,
+      renda: 0,
+      valorSolicitado: null,
+    })
+    this.exibirMsgCampoObrigatorio = false
+  }
+
+  onMessage(propostaAtualizada: any): void {
+    var proposta = JSON.parse(propostaAtualizada.body) as Proposta
+    const index = this.propostas.findIndex(p => p.id === proposta.id);
+    this.propostas[index] = proposta;
+  }
+
+  setObservacao(observacao: string): void {
+    this.observacao = observacao;
+  }
+}
---
+++ cadastro-web/src/app/model/proposta.ts
@@ -0,0 +1,13 @@
+export class Proposta {
+    id: number = 0
+    nome: string = ''
+    sobrenome: string = ''
+    telefone: string = ''
+    cpf: string = ''
+    renda: number = 0
+    valorSolicitado: number = 0
+    valorSolicitadoFmt: string = ''
+    prazoPagamento: number = 0
+    aprovada: boolean = false
+    observacao: string = ''
+}
