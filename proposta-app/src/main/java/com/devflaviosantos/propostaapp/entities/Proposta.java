7efb984196e3fa7a73f960c335d4930b09d8b2bd
Flavio Santos <devflaviosantos@gmail.com>
2025-09-06 13:44:25 -0300

    [inicio do projeto

---
+++ proposta-app/src/main/java/com/devflaviosantos/propostaapp/entities/Proposta.java
@@ -0,0 +1,37 @@
+package com.devflaviosantos.propostaapp.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_proposta")
+public class Proposta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double valorSolicitado;
+
+    private int prazoPagamento;
+
+    private Boolean aprovada;
+
+    private boolean integrada;
+
+    private String Observacao;
+    @OneToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+
+
+
+
+
+}
---
+++ proposta-app/src/main/java/com/devflaviosantos/propostaapp/entities/Usuario.java
@@ -0,0 +1,31 @@
+package com.devflaviosantos.propostaapp.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_usuario")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String sobrenome;
+
+    private String cpf;
+
+    private String telefone;
+
+    private double renda;
+
+    @OneToOne(mappedBy = "usuario")
+    private Proposta proposta;
+}
