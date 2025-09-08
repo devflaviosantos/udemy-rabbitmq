7fdda535ed2fa1dbbad06ddec6f3f5de09731af1
Flavio Santos <devflaviosantos@gmail.com>
2025-09-08 19:18:37 -0300

    Criaçao do obterProposta

--- proposta-app/src/main/java/com/devflaviosantos/propostaapp/controller/PropostaController.java
+++ proposta-app/src/main/java/com/devflaviosantos/propostaapp/controller/PropostaController.java
@@ -4,14 +4,11 @@ import com.devflaviosantos.propostaapp.dtos.PropostaRequestDto;
 import com.devflaviosantos.propostaapp.dtos.PropostaResponseDto;
 import com.devflaviosantos.propostaapp.services.PropostaService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -19,14 +16,17 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 public class PropostaController {
 
     private PropostaService propostaService;
-
-    @PostMapping
-    public ResponseEntity<PropostaResponseDto> salvarProposta(@RequestBody PropostaRequestDto propostaRequestDto) {
+aty<PropostaResponseDto> salvarProposta(@RequestBody PropostaRequestDto propostaRequestDto) {
         PropostaResponseDto responseDto = propostaService.salvarProposta(propostaRequestDto);
         return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}")
                 .buildAndExpand(responseDto.getId())
                 .toUri())
                 .body(responseDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PropostaResponseDto>> obterProposta() {
+         return ResponseEntity.ok(propostaService.obterProposta());
     }
 }
--- proposta-app/src/main/java/com/devflaviosantos/propostaapp/services/PropostaService.java
+++ proposta-app/src/main/java/com/devflaviosantos/propostaapp/services/PropostaService.java
@@ -8,6 +8,8 @@ import com.devflaviosantos.propostaapp.repositories.PropostaRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @AllArgsConstructor
 @Service
 public class PropostaService {
@@ -18,5 +20,9 @@ public class PropostaService {
         propostaRepository.save(proposta);
 
         return PropostaMapper.INSTANCE.convertEntytiToDto(proposta);
+    }
+
+    public List<PropostaResponseDto> obterProposta() {
+        return PropostaMapper.INSTANCE.convertListEntityToListDto(propostaRepository.findAll());
     }
 }
